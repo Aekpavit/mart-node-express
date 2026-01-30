a17954dcd9ba0bf34db795412b200442d4f3c421
wanwanat <gomo372g@gmail.com>
2026-01-30 14:28:15 +0700

    server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const mainRouter = require('./routes/main.routes');
+
+app.use('/',mainRouter)
+
+
+app.listen(8080,()=>{
+    console.log("SERVER : http://localhost:8080");
+})
